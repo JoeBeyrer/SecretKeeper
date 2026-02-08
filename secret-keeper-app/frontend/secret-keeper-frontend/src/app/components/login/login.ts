40ff2fa9c3168100de69fe88d3d99773da9542aa
CFarese <christian@farese.net>
2026-02-07 23:54:49 -0500

    Made a login page with basic functionality

--- secret-keeper-app/frontend/secret-keeper-frontend/src/app/components/login/login.ts
+++ secret-keeper-app/frontend/secret-keeper-frontend/src/app/components/login/login.ts
@@ -1,11 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
 
+export class Login implements OnInit {
+  loginForm: FormGroup;
+  errorMessage: string = '';
+  validCredentials: [string, string][] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private http: HttpClient
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  ngOnInit() {
+    this.http.get('app/components/login/login.txt', { responseType: 'text' }) //getting info from login.txt
+      .subscribe({
+        next: (data) => {
+          try {
+            this.validCredentials = JSON.parse(data);
+            console.log('got the credentials', this.validCredentials);
+          } catch (error) {
+            console.error('something went wrong parsing login.txt', error);
+          }
+        },
+        error: (error) => {
+          console.error('something went wrong loading login.txt', error);
+        }
+      });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      
+      const isValid = this.validCredentials.some(
+        ([validUser, validPass]) => validUser === username && validPass === password
+      );
+      
+      if (isValid) {
+        this.errorMessage = '';
+        console.log('login worked', { username });
+        this.router.navigate(['/messaging']);
+      } else {
+        this.errorMessage = 'Login incorrect';
+        console.log("login didn't work", { username });
+      }
+    } else {
+      this.errorMessage = 'Fill in all fields';
+    }
+  }
 }
