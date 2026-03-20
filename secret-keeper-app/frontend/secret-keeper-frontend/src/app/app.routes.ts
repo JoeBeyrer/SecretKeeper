10a883b2531454b11b84bef849dee132f49b9db7
David Edelstein <dedelstein@ufl.edu>
2026-03-20 13:40:08 -0400

    Update app.routes.ts
    Added /profile route.

--- secret-keeper-app/frontend/secret-keeper-frontend/src/app/app.routes.ts
+++ secret-keeper-app/frontend/secret-keeper-frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Login } from './components/login/login';
 import { Messaging } from './components/messaging/messaging';
 import { Signup } from './components/signup/signup';
 import { PasswordReset } from './components/password-reset/password-reset';
+import { Profile } from './components/profile/profile';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'messaging', component: Messaging },
   { path: 'signup', component: Signup },
-  { path: 'reset-password', component: PasswordReset }
+  { path: 'reset-password', component: PasswordReset },
+  { path: 'profile', component: Profile }
 ];
